562010110310835ec189ee3ec9ca1b7f64f83bb2
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-01 14:12:34 -0500

    Brew formula update for cjwt version v1.0.3

--- Formula/cjwt.rb
+++ Formula/cjwt.rb
@@ -2,16 +2,16 @@
 class Cjwt < Formula
   desc "tool for inspecting jwts"
   homepage "https://github.com/aricart/cjwt"
-  version "1.0.2"
+  version "1.0.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/aricart/cjwt/releases/download/v1.0.2/cjwt-darwin-amd64.zip"
-    sha256 "33269e7663f1c87d1c3e70c1f88286b98af75096fd1f60e3ffa8a548ec9fd0ad"
+    url "https://github.com/aricart/cjwt/releases/download/v1.0.3/cjwt-darwin-amd64.zip"
+    sha256 "1d523e612af300d4287286c386677288d0f3bc88b786a771820622a6cf7f946e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/aricart/cjwt/releases/download/v1.0.2/cjwt-linux-amd64.zip"
-      sha256 "b9673178652da1b68d72e21da75896ebfa071b010b0a040f0b6f4dd697612b01"
+      url "https://github.com/aricart/cjwt/releases/download/v1.0.3/cjwt-linux-amd64.zip"
+      sha256 "ed589d480739ae338904aace270a0276eefa95d940a21af1a40270bf8b4970ef"
     end
   end
 
